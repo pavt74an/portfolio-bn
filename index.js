6c9694b4c798f18dd656c242a44fb5fd7b37a8e4
Paweesuda-Thippayanasa <paweesuda.thippayanasa@gmail.com>
2025-01-30 00:50:50 +0700

    update connection

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
 const mongoose = require("mongoose");
 const express = require("express");
 const { Project, ProjectDetail, ProjectStatus } = require('./src/model/projectSchema');
 const router = require('./src/route/projectRoute');
-require('dotenv').config()
 
 console.log(process.env.MONGO_URL);
 console.log(process.env.PORT_DB);
@@ -14,19 +14,14 @@ app.use(express.json());
 const mongoURI = process.env.MONGO_URL;
 
 const PORT = process.env.PORT_DB ;
+mongoose.connect(mongoURI)
+  .then(() => console.log(" Connected to MongoDB Atlas"))
+  .catch(err => console.error(" MongoDB Connection Error:", err));
 
-console.log(mongoURI);
-console.log(PORT);
+
+
 
 // routes
 app.use('/port', router);
 
 
-app.listen(PORT,  async () =>{
-    try {
-        await mongoose.connect(mongoURI);
-        console.log(`Server is running on port ${PORT}`);
-    } catch (error) {
-        console.log(`Error: ${error}`);
-    }
-})
