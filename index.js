02c155b158686c0f3843f57bd527aa7f948b548b
Paweesuda-Thippayanasa <paweesuda.thippayanasa@gmail.com>
2025-01-30 01:03:12 +0700

    update connection

--- index.js
+++ index.js
@@ -14,14 +14,19 @@ app.use(express.json());
 const mongoURI = process.env.MONGO_URL;
 
 const PORT = process.env.PORT_DB ;
-mongoose.connect(mongoURI)
-  .then(() => console.log(" Connected to MongoDB Atlas"))
-  .catch(err => console.error(" MongoDB Connection Error:", err));
 
-
-
+console.log(mongoURI);
+console.log(PORT);
 
 // routes
 app.use('/port', router);
 
 
+app.listen(PORT,  async () =>{
+    try {
+        await mongoose.connect(mongoURI);
+        console.log(`Server is running on port ${PORT}`);
+    } catch (error) {
+        console.log(`Error: ${error}`);
+    }
+})
